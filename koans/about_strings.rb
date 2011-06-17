dfc2587f5c16a97c6b46ce2946a0e19970fe7887
Stanislav Savulchik <savulchik@savulchik-desktop.(none)>
2011-06-17 08:15:39 +0700

    Выполнено задание по символам

--- koans/about_strings.rb
+++ koans/about_strings.rb
@@ -143,10 +143,10 @@ EOS
 
   in_ruby_version("1.8") do
     def test_in_ruby_1_8_single_characters_are_represented_by_integers
-      assert_equal __, ?a
-      assert_equal __, ?a == 97
+      assert_equal 97, ?a
+      assert_equal true, ?a == 97
 
-      assert_equal __, ?b == (?a + 1)
+      assert_equal true, ?b == (?a + 1)
     end
   end
 
@@ -160,7 +160,7 @@ EOS
 in_ruby_version("1.8") do
     def test_in_ruby_1_8_you_can_get_a_single_character_from_a_string
       string = "Bacon, lettuce and tomato"
-      assert_equal __, string[1]
+      assert_equal 97, string[1]
 
       # Surprised?
     end
